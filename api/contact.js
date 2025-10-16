5987172f0a98abc1f35db77e7cc0eca2048f30eb
Franco <fkasmenko@gmail.com>
2025-10-15 21:47:55 -0300

    Se agrega funcionalidad al formulario de contacto 4

--- api/contact.js
+++ api/contact.js
@@ -28,6 +28,7 @@ export default async function handler(req, res) {
         });
         res.status(200).json({ ok: true });
     } catch (err) {
+        console.error(err);
         res.status(500).json({ ok: false, error: "Error al enviar correo" });
     }
 }
